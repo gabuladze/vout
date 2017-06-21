888da6d3fc36db3be594a676dbf482c0bbe5b325
Giorgi Abuladze <g.abuladze@live.com>
2017-06-21 20:09:45 +0400

    Capture the transmitted id

--- client/src/app/pages/poll-details/poll-details.component.ts
+++ client/src/app/pages/poll-details/poll-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { PollsService } from "../../services/polls.service";
 
 @Component({
   selector: 'app-poll-details',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./poll-details.component.css']
 })
 export class PollDetailsComponent implements OnInit {
+  id: String;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private _polls: PollsService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = params['id'];
+    });
   }
 
 }
