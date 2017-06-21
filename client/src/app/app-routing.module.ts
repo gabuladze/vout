f0e49b7779ed3d9aec5f06f8527b82fab256f176
Giorgi Abuladze <g.abuladze@live.com>
2017-06-21 20:01:58 +0400

    Added view route for poll details

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { PollDetailsComponent } from "./pages/poll-details/poll-details.component";
 import { PollsComponent } from './pages/polls/polls.component';
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'polls',
     component: PollsComponent
+  },
+  {
+    path: 'polls/:id/view',
+    component: PollDetailsComponent
   }
 ];
 
