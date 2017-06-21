c15c7bc7e90332128b63a8f6996aacf0c434c4c0
Giorgi Abuladze <g.abuladze@live.com>
2017-06-21 16:36:19 +0400

    Implemented getAllPolls method for retrieving all polls

--- client/src/app/services/polls.service.ts
+++ client/src/app/services/polls.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class PollsService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  /**
+   * Retrieve the list of all polls from server
+   */
+  getAllPolls() {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    return this.http.get('http://localhost:3500/api/polls')
+      .map(res => res.json());
+  }
 }
