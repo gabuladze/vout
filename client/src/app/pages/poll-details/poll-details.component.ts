5372adf67cb9cb43cd047dba1f9eabd7dec5ff10
Giorgi Abuladze <g.abuladze@live.com>
2017-06-21 19:59:57 +0400

    Initial commit

---
+++ client/src/app/pages/poll-details/poll-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-poll-details',
+  templateUrl: './poll-details.component.html',
+  styleUrls: ['./poll-details.component.css']
+})
+export class PollDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
