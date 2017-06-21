7b48de01037f0ee3f5da6ce9c9c2cd813dd66d83
Giorgi Abuladze <g.abuladze@live.com>
2017-06-21 16:29:26 +0400

    Added /api/polls get route for retrieving poll listing

---
+++ api/routes/polls.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Poll.js');
+
+router.get('/api/polls', (req, res, next) => {
+  //Get all polls
+  Poll.find()
+    .select({
+      title: 1,
+      _id: 1
+    })
+    .sort({
+      title: -1
+    })
+    .exec(function (err, polls) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to retrieve Polls!' });
+      } else {
+        return res.json({ polls: polls });
+      }
+    });
+});
