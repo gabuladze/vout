5140f6c0197b95a4fb625065e6ec1988e787e093
Giorgi Abuladze <g.abuladze@live.com>
2017-06-25 20:09:29 +0400

    Moved login to separate component

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [AuthService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _auth: AuthService) { }
 
   ngOnInit() {
   }
--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "angular2-google-login";
-import { LoginService } from "../../services/login.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'],
-  providers: [LoginService, AuthService]
+  styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private _login: LoginService) { }
+  constructor() { }
 
   ngOnInit() {
   }
