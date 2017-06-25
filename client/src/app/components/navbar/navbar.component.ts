a0d3ac6df60c406edc04f305cee8cf9cb3ab41c2
Giorgi Abuladze <g.abuladze@live.com>
2017-06-25 13:18:06 +0400

    Changed auth to login

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from "../../services/auth.service";
+import { AuthService } from "angular2-google-login";
+import { LoginService } from "../../services/login.service";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  providers: [AuthService]
+  providers: [LoginService, AuthService]
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private _auth: AuthService) { }
+  constructor(private _login: LoginService) { }
 
   ngOnInit() {
   }
