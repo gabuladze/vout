b9740305b5940eaa9af183ba71c15bf8775fcaf9
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 15:22:30 +0400

    Implemented onSubmit method

--- client/src/app/pages/create-poll/create-poll.component.ts
+++ client/src/app/pages/create-poll/create-poll.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { FlashMessagesService } from "angular2-flash-messages";
 import { PollsService } from "../../services/polls.service";
+import { LoginService } from "../../services/login.service";
 
 @Component({
   selector: 'app-create-poll',
   templateUrl: './create-poll.component.html',
   styleUrls: ['./create-poll.component.css'],
-  providers: [PollsService]
+  providers: [PollsService, LoginService]
 })
 export class CreatePollComponent implements OnInit {
   title: string;
   options: any;
 
-  constructor(private _polls: PollsService) { }
+  constructor(
+    private _polls: PollsService,
+    private _login: LoginService,
+    private _flashMessage: FlashMessagesService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.options = [];
@@ -34,6 +43,15 @@ export class CreatePollComponent implements OnInit {
   }
 
   onSubmit() {
+    let userId = this._login.getProfile()['id'];
+    this._polls.createPoll(this.title, this.options, userId).subscribe(data => {
+      if (data.success) {
+        this._flashMessage.show('Poll has been added!', { cssClass: 'alert-success', timeout: 5000 });
+        this.router.navigate(['/polls']);
+      } else {
+        this._flashMessage.show(data.message, { cssClass: 'alert-danger', timeout: 5000 });
+      }
+    });
   }
 
 }
