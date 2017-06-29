969c577690bf4084838ddf4108767e165509d7e0
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 16:38:48 +0400

    Initial Commit

---
+++ client/src/app/pages/user-polls/user-polls.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-polls',
+  templateUrl: './user-polls.component.html',
+  styleUrls: ['./user-polls.component.css']
+})
+export class UserPollsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
