b8eeaa51c62b1e5659f666d73400c5a43491a429
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 18:30:47 +0400

    Added auth guard to polls/create, /polls/user routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from "./guards/auth.guard";
+
 import { CreatePollComponent } from "./pages/create-poll/create-poll.component";
 import { PollDetailsComponent } from "./pages/poll-details/poll-details.component";
 import { PollsComponent } from './pages/polls/polls.component';
@@ -22,15 +24,18 @@ const routes: Routes = [
   },
   {
     path: 'polls/create',
-    component: CreatePollComponent
+    component: CreatePollComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: 'polls/users/:id',
-    component: UserPollsComponent
+    component: UserPollsComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
 @NgModule({
+  providers: [AuthGuard],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
