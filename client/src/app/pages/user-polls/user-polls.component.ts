9f1e80a13f9ae951b0e7eb83791fe8a87090dfaf
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 17:41:00 +0400

    Added polls service

--- client/src/app/pages/user-polls/user-polls.component.ts
+++ client/src/app/pages/user-polls/user-polls.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { PollsService } from "../../services/polls.service";
 
 @Component({
   selector: 'app-user-polls',
   templateUrl: './user-polls.component.html',
-  styleUrls: ['./user-polls.component.css']
+  styleUrls: ['./user-polls.component.css'],
+  providers: [PollsService]
 })
 export class UserPollsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _polls: PollsService) { }
 
   ngOnInit() {
   }
