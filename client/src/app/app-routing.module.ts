356e5a4c93396eec74b06cc03e33d54284700ea3
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 13:33:53 +0400

    Added polls/create route

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CreatePollComponent } from "./pages/create-poll/create-poll.component";
 import { PollDetailsComponent } from "./pages/poll-details/poll-details.component";
 import { PollsComponent } from './pages/polls/polls.component';
 
@@ -17,6 +18,10 @@ const routes: Routes = [
   {
     path: 'polls/:id/view',
     component: PollDetailsComponent
+  },
+  {
+    path: 'polls/create',
+    component: CreatePollComponent
   }
 ];
 
