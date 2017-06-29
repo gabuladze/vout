1e187f4dffc3a901439d6a2700a0655820ed3c10
Giorgi Abuladze <g.abuladze@live.com>
2017-06-29 17:48:39 +0400

    Fetch polls for logged in user on component init

--- client/src/app/pages/user-polls/user-polls.component.ts
+++ client/src/app/pages/user-polls/user-polls.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { PollsService } from "../../services/polls.service";
+import { LoginService } from "../../services/login.service";
+import { FlashMessagesService } from "angular2-flash-messages";
 
 @Component({
   selector: 'app-user-polls',
   templateUrl: './user-polls.component.html',
   styleUrls: ['./user-polls.component.css'],
-  providers: [PollsService]
+  providers: [PollsService, LoginService]
 })
 export class UserPollsComponent implements OnInit {
+  userId: string;
+  polls: any;
 
-  constructor(private _polls: PollsService) { }
+  constructor(
+    private _polls: PollsService,
+    private _login: LoginService,
+    private _flashMessage: FlashMessagesService
+  ) { }
 
   ngOnInit() {
+    this.userId = this._login.getProfile()['id'];
+    this._polls.getUserPolls(this.userId).subscribe(data => {
+      if (data.success) {
+        this.polls = data.polls;
+      } else {
+        this.polls = [];
+        this._flashMessage.show(data.message, { cssClass: 'alert-danger', timeout: 5000 });
+      }
+    });
   }
 
 }
