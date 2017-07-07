86dffa2d892415189d2777061bb5517402baf848
Giorgi Abuladze <g.abuladze@live.com>
2017-07-07 20:17:37 +0400

    Temporarily fixed invalid token bug

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from "./services/login.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [LoginService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  constructor(private _login: LoginService) {}
+
+  ngOnInit() {
+    // TEMPORARY SOLUTION FOR CORRECT BEHAVIOR OF APP
+    
+    this._login.clearLocalStorage();
+  }
 }
